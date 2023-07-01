a4d614cac7338a2d3e9381a9145b73d1f8d69a18
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-01 09:42:38 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to the Heart Disease Risk Calculator!
+
+This chatbot is here to help you understand your risk of heart disease :heart:
+
+Answer the questions one by one, and if you have any questions for the chatbot, go ahead and ask it.
+
+Please contact us if you have any questions!
+"""
