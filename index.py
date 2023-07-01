cd797c6e4f3ae6e04baf26c25ff22b9a19ea2425
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-01 09:54:22 -0700

    update text + add empty chatbot class

--- index.py
+++ index.py
@@ -7,9 +7,12 @@ import streamlit as st
 """
 # Welcome to the Heart Disease Risk Calculator!
 
-This chatbot is here to help you understand your risk of heart disease :heart:
+This chatbot is here to help you understand your risk of heart disease. This is a serious topic; please do not use this calculator for fun.
 
-Answer the questions one by one, and if you have any questions for the chatbot, go ahead and ask it.
+Answer the questions one by one, and if you have any questions for the chatbot, go ahead and ask them. Once you get your results back, we will provide you with some next steps and resources to follow.
 
 Please contact us if you have any questions!
 """
+
+class Chatbot:
+  return 0
