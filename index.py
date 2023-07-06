3039889a5189d9810a7eee5bdcb2d383a65af50d
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-06 13:09:24 -0700

    changed libraries imported

--- index.py
+++ index.py
@@ -1,9 +1,4 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-import panel as on
 import openai 
 
 # pip install streamlit-chat  
