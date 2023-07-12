74c8471d3ae77382cd3f0f84f1c3bc46d43f27f7
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-11 23:03:09 -0700

    update chatbot

--- index.py
+++ index.py
@@ -16,11 +16,14 @@ Please contact us if you have any questions!
 
 openai.api_key = st.secrets["openai"]
 
-def generate_response(prompt):
+ef generate_response(prompt):
     completions = openai.Completion.create(
+        engine = "text-davinci-003",
         prompt = prompt,
-        model = "gpt-3.5-turbo",
-        temperature = 0,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature=0.5,
     )
     message = completions.choices[0].text
     return message 
