111bf67f5a58e46d5627900d8afeaab8ae0493a6
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-26 11:24:44 -0700

    print to console

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from streamlit_chat import message
 from framingham10yr.framingham10yr import framingham_10year_risk
 
 result = framingham_10year_risk(sex="male", age=26, total_cholesterol=152, hdl_cholesterol=70,  systolic_blood_pressure=130, smoker=True, blood_pressure_med_treatment=False)
-"""result["status"], result["message"]"""
+st.write(result["status"], result["message"])
 
 """
 # Welcome to the Heart Disease Risk Calculator!
