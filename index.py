f5e92554948dca2f7a7ea31c1bfd64208af4200b
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-27 11:55:50 -0700

    fix errors

--- index.py
+++ index.py
@@ -110,15 +110,7 @@ def check_for_risk():
    
       result = framingham_10year_risk(gender, age, total_cholesterol, hdl_cholesterol, systolic_bp, smoker, bp_treatment)
       st.write(result)
-      if "OK" not in result['message']:
-         errors = ' '.join(result['errors'])
-         st.session_state.context.append({'role':'system', 'content':"""We could not proceeed due to these errors: """ + errors + """. 
-
-         Please correct the errors with the user and print out the final JSON again.
-      
-         """})
-         st.session_state.generated.append(get_response_from_messages(st.session_state['context']))
-      else:
+      if "200" in result['status']:
          percent = result['percent_risk']
          st.write(percent)
          st.session_state.context.append({'role':'system', 'content':"""The user's percent risk is """ + percent + """. Please restate 
@@ -128,6 +120,15 @@ def check_for_risk():
    
          If the percent is very high, console the user after giving them the news and assure them everything will be okay.
          
+         """})
+         st.session_state.generated.append(get_response_from_messages(st.session_state['context']))
+      elif "422" in result['status']:
+         errors = ' '.join(result['errors'])
+         st.write(errors)
+         st.session_state.context.append({'role':'system', 'content':"""We could not proceeed due to these errors: """ + errors + """. 
+
+         Please correct the errors with the user and print out the final JSON again.
+      
          """})
          st.session_state.generated.append(get_response_from_messages(st.session_state['context']))
 
