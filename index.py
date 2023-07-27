98511d00800d219af19d9d9a8fb6f85f44e0cc9b
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-26 23:11:15 -0700

    show chatbot response

--- index.py
+++ index.py
@@ -143,4 +143,7 @@ if "{" in st.session_state['generated'][-1]:
 
    result = framingham_10year_risk(gender, age, total_cholesterol, hdl_cholesterol, systolic_bp, smoker, bp_treatment)
    st.write(result['status'], result['message'], result['percent_risk'])
-   
+   st.session_state.context.append({'role':'user', 'content':"My percent risk is", result['percent_risk'], """. Can you please restate my 
+   percent calculated risk and tell me
+   what the next steps are? Should I go see a health professional?? What should I do?"})
+   st.session_state.generated.append(get_response_from_messages(st.session_state['context']))
