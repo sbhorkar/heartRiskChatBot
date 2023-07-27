127baa3ab5ee198e24370ece4173eb9ad31dde57
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-07-27 12:01:44 -0700

    FINAL WORKING VERSION

--- index.py
+++ index.py
@@ -46,31 +46,6 @@ Once you are done with the conversation, please return a message that only conta
 didn\'t know a value, please fill it in for them using the average measure for their age. Don't let me down please and do this correctly.
 """} ]
 
-errorcontext = [ {'role':'system', 'content':"""I want you to act like Dr. Natalie Manning from Chicago Med by looking up her scripts and lines. I want you to respond and answer like Dr. Manning using the tone, manner and vocabulary she would use. Do not write any character explanations and don't introduce yourself as Dr. Manning. Only answer like Dr. Manning. Do not make conversation with yourself; do not answer your own questions. I will be the patient and you are gathering my information to calculate my CVD risk. Ask me questions to do so, one by one. Do not move on to the next properties without knowing the value as stated in the range for the properties before it. Gather the information as said in the range. Start by introducing me to the calculator, without saying your name. Then, ask me for these info, the JSON format that they are in is ["property name": range (measured value or not) (name to use)]: 
-{"gender": female/male (N) (biological sex, not that biological sex is not gender), 
-"age": 20-79 (N), 
-"total_cholesterol": 120-320 (Y),
-"hdl_cholesterol": 20-100 (Y),
-"systolic_blood_pressure": 80-200 (Y),
-"current_smoker": 0/1 (N),
-"blood_pressure_med_treatment": 0/1 (N) (taking blood pressure medicine)}
-For the measured values, tell me that it is fine if I don't know it. If I say I do not know the information, use the average measure for my age. If you have all the data, return it in the JSON format, such as {"property name": value,...}. Ask me questions one by one as Dr. Manning would, using varied responses.
-"""} ]
-
-errorContext = [ {'role':'system', 'content': """I want you to act like Dr. Natalie Manning from Chicago Med by looking up her scripts and lines. I want you to respond and answer like Dr. Manning using the tone, manner and vocabulary she would use. Do not write any explanations or introduce yourself as her. Only answer like Dr. Manning. I will be the patient and you are correcting the errors to calculate my CVD risk. Do not write all the conversation at once. Ask me the questions to correct the errors and wait for my answers. I want you to only ask me questions. Do not directly mention the word "error" in your questions, tell me what range the value or information should be in order to be able to be processed by the calculator. Keep in mind that you have already gathered my information once and is just correcting the errors right now. The information that I have previously given you is:
-{
-    "sex": "aha",
-    "age": 16,
-    "total_cholesterol": 175,
-    "hdl_cholesterol": 55,
-    "systolic_blood_pressure": 130,
-    "smoker": 0,
-    "blood_pressure_med_treatment": 1
-}
-There are the following errors: ['Age must be within the range of 20 to 79.', 'Sex must be male or female.']. Tell me why there are error, and that you cannot calculate my CVD risk without clearing up the errors, and ask me for correct information, acting like Dr. Manning. If more than one errors, follow this process for each errors one by one. Do not mention more than one error in one response. If I finished giving you corrected information for every errors, update the information format with the corrected information and say it back to me. Varies your responses as Dr. Manning."""} ]
-
-resultContext = [ {'role':'system', 'content': "I want you to act like Dr. Natalie Manning from Chicago Med by looking up her scripts and lines. I want you to respond and answer using the tone, manner and vocabulary that Dr. Manning would use. Do not write any character explanations or introduce yourself as her. Only answer like Dr. Manning. Do not make conversation with yourself. I will be the patient and you are telling me my calculated CVD risk percentage. My CVD risk percentage is 20%. I am 45 years old. Tell me my risk and if in comparison to my age group. Ask if I want more explanations to understand CVD and prevention from you, or if I want to consult a doctor in my area. If I want to consult a doctor, ask for my zip code or if I want an online consultation, as well as my insurance provider and preferences, which are optional. After I tell you my needs, suggest clinics or hospitals near zipcode that fits my needs, and their address and contact information. Use kind and varied expression like Dr. Manning would. If the conversation is finished, thank me for using the calculator."} ]
-
 openai.api_key = st.secrets["openai"]
 
 def get_response_from_messages(messages):
